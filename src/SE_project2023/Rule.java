cf25e15b616855d21b98fbb28304c174666992c0
sirclxrd <cdeangelis1909@gmail.com>
2023-11-22 15:01:50 +0100

    Revert "RuleEma"
    This reverts commit ba7b4c90b67500ef6646c3b8e3cb165193d19aa1.

--- src/SE_project2023/Rule.java
+++ src/SE_project2023/Rule.java
@@ -1,63 +1,38 @@
 package SE_project2023;
 
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
 /**
  *
- * @author emanu
+ * @author giova
  */
-public class Rule {
-    String name;
-    Action action;
-    Trigger trigger;
+class Rule {
+    String Name;
 
-    //Costruttori
-    public Rule() {}
-
-    public Rule(String name) {
-        this.name = name;
+    public Rule(String Name) {
+        this.Name = Name;
     }
 
-    public Rule(String name, Action action, Trigger trigger) {
-        this.name = name;
-        this.action = action;
-        this.trigger = trigger;
-    }
-
-    //Getter
-    public Action getAction() {
-        return action;
-    }
-
-    public Trigger getTrigger() {
-        return trigger;
-    }
-    
-    public String getName() {
-        return name;
-    }
-    
-    //Setter
-    public void setAction(Action action) {
-        this.action = action;
-    }
-
-    public void setTrigger(Trigger trigger) {
-        this.trigger = trigger;
-    }
-
-    public void setName(String Name) {
-        this.name = Name;
+    Rule() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     @Override
-        public String toString() {
-            return "Regola : " + name;
-        }
-        
-    public boolean isVerifiedRule(){
-        if(this.trigger.isVerified()){
-            this.action.fire();
-            return true;
-        }
-        return false;
+    public String toString() {
+        return "Regola "+ Name+" :";
     }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    
 }
