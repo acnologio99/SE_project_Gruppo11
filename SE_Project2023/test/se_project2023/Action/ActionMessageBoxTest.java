b4891acb894a4ffa7d062388aec4c6021314e23f
sirclxrd <cdeangelis1909@gmail.com>
2023-11-25 18:14:54 +0100

    ActionMessageBox tests

--- SE_Project2023/test/se_project2023/Action/ActionMessageBoxTest.java
+++ SE_Project2023/test/se_project2023/Action/ActionMessageBoxTest.java
@@ -16,15 +16,13 @@ import static org.junit.Assert.*;
  *
  * @author chris
  */
-public class ActionMessageBoxTest  {
+public class ActionMessageBoxTest {
+
     ActionMessageBox instance;
-    
-   
-   
-    
+
     public ActionMessageBoxTest() {
     }
-    
+
     @Before
     public void setUp() {
         instance = new ActionMessageBox("");
@@ -34,28 +32,27 @@ public class ActionMessageBoxTest  {
      * Test of getMsg method, of class ActionMessageBox.
      */
     @Test
-    public void testGetSetMsg(){
+    public void testGetSetMsg() {
         System.out.println("getsetMsg");
-        
+
         /* first test case expected msg*/
         String msg = "Hello";
         instance.setMsg(msg);
-        String expResult = "Hello"; 
+        String expResult = "Hello";
         String result = instance.getMsg();
         assertEquals(expResult, result);
-        
+
         /*second test case expected "" when msg is null*/
         msg = null;
         instance.setMsg(msg);
         result = instance.getMsg();
         assertEquals("", result);
-        
+
         /*third case with constructor*/
         ActionMessageBox instance2 = new ActionMessageBox(null);
         result = instance.getMsg();
         assertEquals("", result);
-        
-        
+
     }
 
     /**
@@ -68,60 +65,54 @@ public class ActionMessageBoxTest  {
         boolean expResult = false;
         boolean result = instance.isFired();
         assertEquals(expResult, result);
-     
+
         Platform.runLater(() -> {
             instance.setMsg("Messaggio Test");
-            instance.fire();        
+            instance.fire();
             assertTrue(instance.isFired());
-            
-            
-            
+
         });
         try {
             Thread.sleep(500); //devo aspettare che il metodo viene richiamato sul thread e che abbia il tempo di eseguire il test
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        
-        
-        
-        
-        
+
     }
 
     /**
      * Test of add method, of class ActionMessageBox.
      */
-    @Test(expected=UnsupportedOperationException.class)
+    @Test(expected = UnsupportedOperationException.class)
     public void testAdd() {
         System.out.println("add");
-        
+
         instance.add();
-        
+
     }
 
     /**
      * Test of remove method, of class ActionMessageBox.
      */
-    @Test(expected=UnsupportedOperationException.class)
+    @Test(expected = UnsupportedOperationException.class)
     public void testRemove() {
         System.out.println("remove");
-        
+
         instance.remove();
-        
+
     }
 
     /**
      * Test of getChild method, of class ActionMessageBox.
      */
-    @Test(expected=UnsupportedOperationException.class)
+    @Test(expected = UnsupportedOperationException.class)
     public void testGetChild() {
         System.out.println("getChild");
-        
+
         Action expResult = null;
         Action result = instance.getChild();
         assertEquals(expResult, result);
-        
+
     }
 
     /**
@@ -130,24 +121,24 @@ public class ActionMessageBoxTest  {
     @Test
     public void testToString() {
         System.out.println("toString");
-       
+
         String expResult = "ActionMessageBox: ";
         String result = instance.toString();
         assertEquals(expResult, result);
-        
+
     }
 
     /**
      * Test of isFired method, of class ActionMessageBox.
      */
-   @Test //the method is tested in testFire() too;
+    @Test //the method is tested in testFire() too;
     public void testIsFired() {
         System.out.println("isFired");
-       
+
         boolean expResult = false;
         boolean result = instance.isFired();
         assertEquals(expResult, result);
-        
+
     }
-    
+
 }
