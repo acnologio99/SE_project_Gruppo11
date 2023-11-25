c3a02860549c651e6641bfc0822c72611eecf126
sirclxrd <cdeangelis1909@gmail.com>
2023-11-25 12:13:30 +0100

    TimeTriggerTest

---
+++ SE_Project2023/test/se_project2023/Trigger/TimeTriggerTest.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package se_project2023.Trigger;
+
+import SE_project2023.Trigger.TimeTrigger;
+import java.time.LocalTime;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author chris
+ */
+public class TimeTriggerTest {
+    
+    public TimeTriggerTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+
+    /**
+     * Test of getTimeOfDay method, of class TimeTrigger.
+     */
+    @Test
+    public void testGetTimeOfDay() {
+        System.out.println("getTimeOfDay");
+        TimeTrigger instance = null;
+        LocalTime expResult = null;
+        LocalTime result = instance.getTimeOfDay();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setTimeOfDay method, of class TimeTrigger.
+     */
+    @Test
+    public void testSetTimeOfDay() {
+        System.out.println("setTimeOfDay");
+        LocalTime timeOfDay = null;
+        TimeTrigger instance = null;
+        instance.setTimeOfDay(timeOfDay);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of isVerified method, of class TimeTrigger.
+     */
+    @Test
+    public void testIsVerified() {
+        System.out.println("isVerified");
+        TimeTrigger instance = null;
+        boolean expResult = false;
+        boolean result = instance.isVerified();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
